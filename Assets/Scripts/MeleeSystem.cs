f296ca2148feb9388e954a66117d615f51a93a20
nightskin <39378941+nightskin@users.noreply.github.com>
2022-11-01 19:03:43 -0400

    fixed attacking glitch

--- Assets/Scripts/MeleeSystem.cs
+++ Assets/Scripts/MeleeSystem.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class MeleeSystem : MonoBehaviour
 {
-    [Range(0, 1)] public float wpnSensitivity;
+    [SerializeField] FirstPersonPlayer player;
+    [Range(0, 1)] [SerializeField] float wpnSensitivity;
     [SerializeField] Transform armPivot;
-    [SerializeField] float atkAngle;
-    
+    [SerializeField] float cooldown = 1;
+
+    float cooldownTimer = 0;
+    public float atkAngle;
+    Animator animator;
     bool isAttacking;
     Vector2 defSensitivity;
-    FirstPersonPlayer player;
-    public Animator animator;
-
     Vector2 axis = new Vector2();
-    RaycastHit hit;
 
     void Start()
     {
-        player = GetComponent<FirstPersonPlayer>();
+        animator = GetComponent<Animator>();
         player.actions.Attack.performed += Attack_performed;
         player.actions.Attack.canceled += Attack_canceled;
         defSensitivity = player.sensitivity;
@@ -39,25 +39,25 @@ public class MeleeSystem : MonoBehaviour
 
     void Update()
     {
-
-
-        if (isAttacking)
+        if(isAttacking)
         {
             axis = player.actions.Look.ReadValue<Vector2>();
-            if(axis.x  != 0 && axis.y != 0) atkAngle = Mathf.Atan2(axis.x, -axis.y) * Mathf.Rad2Deg;
-            armPivot.localEulerAngles = new Vector3(0, 0, atkAngle);
-            animator.SetInteger("r", 180);
+            if(axis.magnitude > 0)
+            {
+                atkAngle = Mathf.Atan2(axis.x, -axis.y) * Mathf.Rad2Deg;
+                animator.SetInteger("r", 180);
+            }
         }
-        else
-        {
-            animator.SetInteger("r", 0);
-            armPivot.localEulerAngles = new Vector3(0, 0, 0);
-        }
+    }
 
-
-
-
-
+    public void SetAttackAngle()
+    {
+        armPivot.localEulerAngles = new Vector3(0, 0, atkAngle);
+    }
+    public void ResetArm()
+    {
+        animator.SetInteger("r", 0);
+        armPivot.localEulerAngles = new Vector3(0, 0, 0);
     }
 
 }
