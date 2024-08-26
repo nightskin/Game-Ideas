08eae6d637f151c70dd70850294bcc982850db2f
nightskin <39378941+nightskin@users.noreply.github.com>
2024-08-26 13:53:27 -0400

    fixed UV problem with DungeonMesh.cs

--- Assets/Scripts/Generators/DungeonMesh.cs
+++ Assets/Scripts/Generators/DungeonMesh.cs
@@ -370,7 +370,7 @@ public class DungeonMesh : MonoBehaviour
 
     GridPoint[,,] grid = null;
     List<Vector3> verts;
-    List<Vector3> normals;
+    List<Vector2> uvs;
     List<int> tris;
     int buffer;
     Mesh mesh;
@@ -497,7 +497,7 @@ public class DungeonMesh : MonoBehaviour
 
         grid = new GridPoint[gridSize.x, gridSize.y, gridSize.z];
         verts = new List<Vector3>();
-        normals = new List<Vector3>();
+        uvs = new List<Vector2>();
         tris = new List<int>();
         buffer = 0;
 
@@ -620,6 +620,13 @@ public class DungeonMesh : MonoBehaviour
         mesh.vertices = verts.ToArray();
         mesh.triangles = tris.ToArray();
 
+        for(int v = 0; v < verts.Count - 2; v+=3)
+        {
+            Vector2[] uvForTri = GetUVs(verts[v], verts[v + 1], verts[v + 2]);
+            uvs.AddRange(uvForTri);
+        }
+        mesh.uv = uvs.ToArray();
+
         mesh.RecalculateBounds();
         mesh.RecalculateNormals();
         mesh.RecalculateTangents();
@@ -724,5 +731,38 @@ public class DungeonMesh : MonoBehaviour
                 }
             }
         }
+    }
+
+    Vector2[] GetUVs(Vector3 a, Vector3 b, Vector3 c)
+    {
+        Vector3 s1 = b - a;
+        Vector3 s2 = c - a;
+        Vector3 norm = Vector3.Cross(s1, s2).normalized; // the normal
+
+        norm.x = Mathf.Abs(norm.x);
+        norm.y = Mathf.Abs(norm.y);
+        norm.z = Mathf.Abs(norm.z);
+
+        Vector2[] uvs = new Vector2[3];
+        if (norm.x >= norm.z && norm.x >= norm.y) // x plane
+        {
+            uvs[0] = new Vector2(a.z, a.y);
+            uvs[1] = new Vector2(b.z, b.y);
+            uvs[2] = new Vector2(c.z, c.y);
+        }
+        else if (norm.z >= norm.x && norm.z >= norm.y) // z plane
+        {
+            uvs[0] = new Vector2(a.x, a.y);
+            uvs[1] = new Vector2(b.x, b.y);
+            uvs[2] = new Vector2(c.x, c.y);
+        }
+        else if (norm.y >= norm.x && norm.y >= norm.z) // y plane
+        {
+            uvs[0] = new Vector2(a.x, a.z);
+            uvs[1] = new Vector2(b.x, b.z);
+            uvs[2] = new Vector2(c.x, c.z);
+        }
+
+        return uvs;
     }
 }
